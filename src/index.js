c2b3398193ca5c9c50932e5e86d89c8c3e3756aa
melok2207 <132545373+melok2207@users.noreply.github.com>
2025-05-06 12:09:10 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Підключіть Tailwind або інші стилі
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
