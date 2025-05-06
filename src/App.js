a3f5e737e793d20db49b7801fae5969718a45d61
melok2207 <132545373+melok2207@users.noreply.github.com>
2025-05-06 12:08:53 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Gallery from './components/Gallery';
+
+function App() {
+  return (
+    <div className="max-w-4xl mx-auto">
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
